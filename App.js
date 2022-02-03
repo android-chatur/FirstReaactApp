df426125cc1434c67fa9bae7231f43d845d230b7
android-chatur <chatur.patil10@gmail.com>
2022-02-03 19:29:35 +0530

    First comment

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button, StyleSheet,
+  Console,
+  style,
+  ScrollView,
+  Text,
+  View,
+  TextInput,
+  FlatList
+} from 'react-native';
+import { useState } from 'react';
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
 
 export default function App() {
+
+  const [courceGoal, setCourceGoal] = useState([]);
+
+  const [enteredGoal, setenterGoaltText] = useState('');
+
+
+  const GoalinputHandler = enterText => {
+    setenterGoaltText(enterText)
+}
+  const addGoalHandler = () => {
+    setCourceGoal(currentGoals => [...currentGoals, { id: Math.random().toString, value: enteredGoal }])
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+
+<View style={styles.inpucontner}>
+
+<TextInput
+    placeholder='Course Goal'
+    style={styles.input}
+    onChangeText={GoalinputHandler}
+    value={enteredGoal}
+/>
+<Button
+    title='ADD' onPress={addGoalHandler} />
+</View>
+
+      <FlatList
+       
+        data={courceGoal}
+        renderItem={itemData => (
+        <View style={styles.listite}>
+          <Text>{itemData.item.value}</Text>
+         </View>)}
+      ></FlatList>
+
+
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inpucontner: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: "center"
+  },
+  input: { borderTopColor: 'black', borderWidth: 1, padding: 10, width: "80%" },
+  listite: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  }
+
+})
+
+
---
+++ Components/Goalinput;.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { View, TextInput, StyleSheet, Button } from 'react-native';
+
+const Goalinput = props => {
+
+    const [enteredGoal, setenterGoaltText] = useState('');
+
+    const GoalinputHandler = enterText => {
+        setenterGoaltText(enterText)
+    }
+
+    return (<View style={styles.inpucontner}>
+
+        <TextInput
+            placeholder='Course Goal'
+            style={styles.input}
+            onChangeText={GoalinputHandler}
+            value={enteredGoal}
+        />
+        <Button
+            title='ADD' onPress={props.onAddGoal.bind(this,enteredGoal)} />
+    </View>);
+};
+const styles = StyleSheet.create({
+
+    inpucontner: {
+        flexDirection: 'row', justifyContent: 'space-between', alignItems: "center"
+    },
+    input: { borderTopColor: 'black', borderWidth: 1, padding: 10, width: "80%" },
+
+
+})
+
+export default Goalinput;
