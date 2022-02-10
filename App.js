0442b34898ef0af9bbe4cffbd57e47b567cccefc
android-chatur <chatur.patil10@gmail.com>
2022-02-10 18:56:54 +0530

    complete sample demo

--- App.js
+++ App.js
@@ -1,45 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-  Button, StyleSheet,
-  Console,
-  style,
-  ScrollView,
-  Text,
-  View,
-  TextInput,
-  FlatList
-} from 'react-native';
-import { useState } from 'react';
-import GoalItem from './Components/GoalItem';
-import GoalInput from './Components/GoalInput;';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
-  const [courceGoal, setCourceGoal] = useState([]);
-
-
-  const addGoalHandler = goalTitile => {
-    setCourceGoal(currentGoals => [
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
       ...currentGoals,
-      { id: Math.random().toString, value: goalTitile }])
-  }
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
     <View style={styles.screen}>
-
-      { }
-
-      <GoalInput onAddGoal={addGoalHandler}></GoalInput>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler}
+      />
       <FlatList
         keyExtractor={(item, index) => item.id}
-        data={courceGoal}
-        renderItem={itemData => <GoalItem title={itemData.item.value} />}
-
-
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
       />
-
-
-
-
     </View>
   );
 }
@@ -47,9 +52,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-
-
-})
-
-
+  }
+});
--- Components/Goalitem.js
+++ Components/Goalitem.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { View,Text,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const GoalItem = props =>{
-    return (
-    <View style={styles.listite}>
-    <Text>{props.title}</Text>
-  </View>
-    );
-}
+const GoalItem = props => {
+  return (
+    <TouchableOpacity onPress={props.onDelete.bind(this, props.id)} >
+      <View style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-
-const styles=StyleSheet.create({
-    listite: {
-        padding: 10,
-        marginVertical: 10,
-        backgroundColor: '#ccc',
-        borderColor: 'black',
-        borderWidth: 1,
-      }
-})
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default GoalItem;
